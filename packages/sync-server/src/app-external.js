662a4e69c4b50ca85d908ad7f7a2f4686370f127
Luke Taylor <luke@circleleasing.co.uk>
2025-03-03 15:41:41 +0000

    Add endpoint for fetching crypto holdings from Kraken

--- packages/sync-server/src/app-external.js
+++ packages/sync-server/src/app-external.js
@@ -40,3 +40,25 @@ app.post("/t212-balance", async (req, res) => {
         data: json
     });
 });
+
+app.post("/crypto", async (req, res) => {
+    let pairs = req.body.map(holding => `${holding.ticker}GBP`).join(",");
+    const prices = await fetch(`https://api.kraken.com/0/public/Ticker?pair=${pairs}`).then(res => res.json());
+
+    if(prices.error.length > 0){
+        return res.status(500).send(prices.error);
+    }
+
+    const retVal = {};
+    Object.keys(prices.result).forEach((key, i) => {
+        //would love to do exact matching, but it seems Kraken normalises names - e.g. BTCGBP can be returned as XXBTZGBP
+        //however, everything seems to be returned in the same order it's sent, so we can (ugh) match on indices instead
+        const values = prices.result[key];
+        retVal[req.body[i].ticker] = req.body[i].quantity * parseFloat(values.c[0]);
+    })
+
+    res.send({
+        status: 'ok',
+        data: retVal
+    });
+});
